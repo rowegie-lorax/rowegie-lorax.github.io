84d7fada65e5273c93fb4f2acf4993b550e930c3
Rowegie <rowegie.lorax@gmail.com>
2018-07-02 17:21:39 +0800

    api user updates

--- doc/api_project.js
+++ doc/api_project.js
@@ -2,7 +2,7 @@ define({
   "name": "Baseup",
   "version": "0.1.0",
   "description": "Baseup Web API Documentation",
-  "url": "https://baseup.co/api/v1/",
+  "url": "https://baseup.co/api/v1",
   "template": {
     "withCompare": true,
     "withGenerator": true
@@ -12,7 +12,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-07-02T09:18:51.570Z",
+    "time": "2018-07-02T09:20:46.982Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
